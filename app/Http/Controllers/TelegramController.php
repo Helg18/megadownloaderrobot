488acbc8097c632c837e2a2b0115bbb4712f6416
Henry Leon <helg18@gmail.com>
2020-05-23 00:28:50 -0400

    Comienza el flujo de la peticion

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -1,18 +1,58 @@
 <?php
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Objects\Update;
+use App\Repositories\UserRepository;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TelegramController extends Controller
 {
+    /**
+     * @var UserRepository
+     */
+    private UserRepository $userRepository;
+
+    /**
+     * TelegramController constructor.
+     *
+     * @param UserRepository $userRepository
+     */
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     public function index() {
         // Get Update
-        dd(Telegram::getUpdates());
+        $msg = collect(Telegram::getUpdates())->last();
+
+        // Set update Object
+        $update = new Update($msg);
+
         // Validate to user exist
-        // Save User
+        self::createOrValidateUser($update);
+
         // Process Update
         // Response update processed
 
+    }
+
+    private function createOrValidateUser(Update $telegram)
+    {
+        $user = $this->userRepository->getTelegramID($telegram->getFromId());
+
+        if (!isset($user)) {
+            $params = [
+                'telegram_id' => $telegram->getFromId(),
+                'is_bot' => $telegram->isIsBot(),
+                'first_name' => $telegram->getFirstName(),
+                'last_name' => $telegram->getLastName(),
+                'username' => $telegram->getUsername(),
+                'language_code' => $telegram->getLanguageCode()
+            ];
+            $this->userRepository->create($params);
+        }
+
+        return true;
     }
 }
