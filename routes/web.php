7ba4c5238afae4511a6e60ecf2aa700e0d4abbee
Henry Leon <helg18@gmail.com>
2020-05-23 03:49:10 -0400

    Configuracion para Deploy

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/LuTsh9EniT4fCcOnntMHpWm7BxlIY8Ku/webhook', 'TelegramController@index');
+Route::post('/LuTsh9EniT4fCcOnntMHpWm7BxlIY8Ku/webhook', 'TelegramController@index');
