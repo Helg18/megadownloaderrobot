04c69222b36f94de657f23721a463ab51ad8e4c6
Henry Leon <helg18@gmail.com>
2020-05-22 20:47:54 -0400

    Integrando Paquete para Telegram

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        \Telegram\Bot\Laravel\TelegramServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -226,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Telegram' => \Telegram\Bot\Laravel\Facades\Telegram::class,
 
     ],
 
